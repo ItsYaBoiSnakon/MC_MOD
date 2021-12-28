563a17f3bc79c0eff664ca107a7e97a7a5d1f2d5
5kadooble <danieldebrueijs@hotmail.com>
2021-12-28 17:10:14 +0100

    tungsteningot

--- MCMOD/src/main/java/net/mcreator/morestuff/init/MorestuffModItems.java
+++ MCMOD/src/main/java/net/mcreator/morestuff/init/MorestuffModItems.java
@@ -13,6 +13,8 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.BlockItem;
 
+import net.mcreator.morestuff.item.TungstenIngotItem;
+
 import java.util.List;
 import java.util.ArrayList;
 
@@ -20,6 +22,7 @@ import java.util.ArrayList;
 public class MorestuffModItems {
 	private static final List<Item> REGISTRY = new ArrayList<>();
 	public static final Item TUNGSTEN_ORE = register(MorestuffModBlocks.TUNGSTEN_ORE, CreativeModeTab.TAB_BUILDING_BLOCKS);
+	public static final Item TUNGSTEN_INGOT = register(new TungstenIngotItem());
 
 	private static Item register(Item item) {
 		REGISTRY.add(item);
