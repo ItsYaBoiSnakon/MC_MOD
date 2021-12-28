92838a691367652a34fe3912bf4230f980c926cf
Harm Gerritse <harm.gerritse@gmail.com>
2021-12-28 17:19:46 +0100

    orichalcum

--- MCMOD/src/main/java/net/mcreator/morestuff/init/MorestuffModItems.java
+++ MCMOD/src/main/java/net/mcreator/morestuff/init/MorestuffModItems.java
@@ -8,9 +8,12 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.event.RegistryEvent;
 
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.BlockItem;
 
-import net.mcreator.morestuff.item.SilverItem;
+import net.mcreator.morestuff.item.OrichalcumIngotItem;
 
 import java.util.List;
 import java.util.ArrayList;
@@ -18,11 +21,17 @@ import java.util.ArrayList;
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
 public class MorestuffModItems {
 	private static final List<Item> REGISTRY = new ArrayList<>();
-	public static final Item SILVER = register(new SilverItem());
+	public static final Item ORICHALCUM_INGOT = register(new OrichalcumIngotItem());
+	public static final Item ORICHALCUM_ORE = register(MorestuffModBlocks.ORICHALCUM_ORE, CreativeModeTab.TAB_BUILDING_BLOCKS);
+	public static final Item ORICHALCUM_BLOCK = register(MorestuffModBlocks.ORICHALCUM_BLOCK, CreativeModeTab.TAB_BUILDING_BLOCKS);
 
 	private static Item register(Item item) {
 		REGISTRY.add(item);
 		return item;
+	}
+
+	private static Item register(Block block, CreativeModeTab tab) {
+		return register(new BlockItem(block, new Item.Properties().tab(tab)).setRegistryName(block.getRegistryName()));
 	}
 
 	@SubscribeEvent
