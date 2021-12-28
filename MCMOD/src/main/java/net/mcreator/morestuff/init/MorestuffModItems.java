0168963286ff4a312548841ebd5ad34de6b1775f
ItsYaBoiSnakon <suenokeijer@gmail.com>
2021-12-28 17:09:48 +0100

    Tungsten Ore

---
+++ MCMOD/src/main/java/net/mcreator/morestuff/init/MorestuffModItems.java
@@ -0,0 +1,40 @@
+
+/*
+ *    MCreator note: This file will be REGENERATED on each build.
+ */
+package net.mcreator.morestuff.init;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.event.RegistryEvent;
+
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.BlockItem;
+
+import net.mcreator.morestuff.item.SilverItem;
+
+import java.util.List;
+import java.util.ArrayList;
+
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
+public class MorestuffModItems {
+	private static final List<Item> REGISTRY = new ArrayList<>();
+	public static final Item SILVER = register(new SilverItem());
+	public static final Item TUNGSTEN_ORE = register(MorestuffModBlocks.TUNGSTEN_ORE, CreativeModeTab.TAB_BUILDING_BLOCKS);
+
+	private static Item register(Item item) {
+		REGISTRY.add(item);
+		return item;
+	}
+
+	private static Item register(Block block, CreativeModeTab tab) {
+		return register(new BlockItem(block, new Item.Properties().tab(tab)).setRegistryName(block.getRegistryName()));
+	}
+
+	@SubscribeEvent
+	public static void registerItems(RegistryEvent.Register<Item> event) {
+		event.getRegistry().registerAll(REGISTRY.toArray(new Item[0]));
+	}
+}
