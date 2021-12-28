99515ec7a6b72978358fef2f1634bb01f3a1c79e
Harm Gerritse <harm.gerritse@gmail.com>
2021-12-28 20:20:14 +0100

    orichalcum+necrodermis update

--- MCMOD/src/main/java/net/mcreator/morestuff/init/MorestuffModBlocks.java
+++ MCMOD/src/main/java/net/mcreator/morestuff/init/MorestuffModBlocks.java
@@ -10,8 +10,10 @@ import net.minecraftforge.event.RegistryEvent;
 
 import net.minecraft.world.level.block.Block;
 
-import net.mcreator.morestuff.block.TungstenOreBlock;
-import net.mcreator.morestuff.block.TitaniumOreBlock;
+import net.mcreator.morestuff.block.OrichalcumOreBlock;
+import net.mcreator.morestuff.block.OrichalcumBlockBlock;
+import net.mcreator.morestuff.block.NecrodermisOreBlock;
+import net.mcreator.morestuff.block.NecrodermisBlockBlock;
 
 import java.util.List;
 import java.util.ArrayList;
@@ -19,8 +21,10 @@ import java.util.ArrayList;
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
 public class MorestuffModBlocks {
 	private static final List<Block> REGISTRY = new ArrayList<>();
-	public static final Block TUNGSTEN_ORE = register(new TungstenOreBlock());
-	public static final Block TITANIUM_ORE = register(new TitaniumOreBlock());
+	public static final Block ORICHALCUM_ORE = register(new OrichalcumOreBlock());
+	public static final Block ORICHALCUM_BLOCK = register(new OrichalcumBlockBlock());
+	public static final Block NECRODERMIS_ORE = register(new NecrodermisOreBlock());
+	public static final Block NECRODERMIS_BLOCK = register(new NecrodermisBlockBlock());
 
 	private static Block register(Block block) {
 		REGISTRY.add(block);
--- MCMOD/src/main/java/net/mcreator/morestuff/init/MorestuffModItems.java
+++ MCMOD/src/main/java/net/mcreator/morestuff/init/MorestuffModItems.java
@@ -13,8 +13,20 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.BlockItem;
 
-import net.mcreator.morestuff.item.TungstenIngotItem;
-import net.mcreator.morestuff.item.PencilItem;
+import net.mcreator.morestuff.item.OrichalcumSwordItem;
+import net.mcreator.morestuff.item.OrichalcumShovelItem;
+import net.mcreator.morestuff.item.OrichalcumPickaxeItem;
+import net.mcreator.morestuff.item.OrichalcumIngotItem;
+import net.mcreator.morestuff.item.OrichalcumHoeItem;
+import net.mcreator.morestuff.item.OrichalcumAxeItem;
+import net.mcreator.morestuff.item.OrichalcumArmorItem;
+import net.mcreator.morestuff.item.NecrodermisSwordItem;
+import net.mcreator.morestuff.item.NecrodermisShovelItem;
+import net.mcreator.morestuff.item.NecrodermisPickaxeItem;
+import net.mcreator.morestuff.item.NecrodermisItem;
+import net.mcreator.morestuff.item.NecrodermisHoeItem;
+import net.mcreator.morestuff.item.NecrodermisAxeItem;
+import net.mcreator.morestuff.item.NecrodermisArmorItem;
 
 import java.util.List;
 import java.util.ArrayList;
@@ -22,10 +34,30 @@ import java.util.ArrayList;
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
 public class MorestuffModItems {
 	private static final List<Item> REGISTRY = new ArrayList<>();
-	public static final Item TUNGSTEN_ORE = register(MorestuffModBlocks.TUNGSTEN_ORE, CreativeModeTab.TAB_BUILDING_BLOCKS);
-	public static final Item TUNGSTEN_INGOT = register(new TungstenIngotItem());
-	public static final Item PENCIL = register(new PencilItem());
-	public static final Item TITANIUM_ORE = register(MorestuffModBlocks.TITANIUM_ORE, CreativeModeTab.TAB_BUILDING_BLOCKS);
+	public static final Item ORICHALCUM_INGOT = register(new OrichalcumIngotItem());
+	public static final Item ORICHALCUM_ORE = register(MorestuffModBlocks.ORICHALCUM_ORE, CreativeModeTab.TAB_BUILDING_BLOCKS);
+	public static final Item ORICHALCUM_BLOCK = register(MorestuffModBlocks.ORICHALCUM_BLOCK, CreativeModeTab.TAB_BUILDING_BLOCKS);
+	public static final Item NECRODERMIS = register(new NecrodermisItem());
+	public static final Item NECRODERMIS_ORE = register(MorestuffModBlocks.NECRODERMIS_ORE, CreativeModeTab.TAB_BUILDING_BLOCKS);
+	public static final Item NECRODERMIS_BLOCK = register(MorestuffModBlocks.NECRODERMIS_BLOCK, CreativeModeTab.TAB_BUILDING_BLOCKS);
+	public static final Item NECRODERMIS_PICKAXE = register(new NecrodermisPickaxeItem());
+	public static final Item NECRODERMIS_AXE = register(new NecrodermisAxeItem());
+	public static final Item NECRODERMIS_SWORD = register(new NecrodermisSwordItem());
+	public static final Item NECRODERMIS_SHOVEL = register(new NecrodermisShovelItem());
+	public static final Item NECRODERMIS_HOE = register(new NecrodermisHoeItem());
+	public static final Item NECRODERMIS_ARMOR_HELMET = register(new NecrodermisArmorItem.Helmet());
+	public static final Item NECRODERMIS_ARMOR_CHESTPLATE = register(new NecrodermisArmorItem.Chestplate());
+	public static final Item NECRODERMIS_ARMOR_LEGGINGS = register(new NecrodermisArmorItem.Leggings());
+	public static final Item NECRODERMIS_ARMOR_BOOTS = register(new NecrodermisArmorItem.Boots());
+	public static final Item ORICHALCUM_PICKAXE = register(new OrichalcumPickaxeItem());
+	public static final Item ORICHALCUM_AXE = register(new OrichalcumAxeItem());
+	public static final Item ORICHALCUM_SWORD = register(new OrichalcumSwordItem());
+	public static final Item ORICHALCUM_SHOVEL = register(new OrichalcumShovelItem());
+	public static final Item ORICHALCUM_HOE = register(new OrichalcumHoeItem());
+	public static final Item ORICHALCUM_ARMOR_HELMET = register(new OrichalcumArmorItem.Helmet());
+	public static final Item ORICHALCUM_ARMOR_CHESTPLATE = register(new OrichalcumArmorItem.Chestplate());
+	public static final Item ORICHALCUM_ARMOR_LEGGINGS = register(new OrichalcumArmorItem.Leggings());
+	public static final Item ORICHALCUM_ARMOR_BOOTS = register(new OrichalcumArmorItem.Boots());
 
 	private static Item register(Item item) {
 		REGISTRY.add(item);
